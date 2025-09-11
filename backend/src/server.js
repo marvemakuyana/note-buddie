2b756bde7e9c1a483020c652e3fd39464b49c8cf
marvemakuyana <marvellousmakuyana@gmail.com>
2025-09-11 15:30:17 +0200

    fixed cors issues, frontend - WIP

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -9,6 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,6 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 const App = () => {
   return (
     <div data-theme="retro">
-      <button className="btn">My Buttons</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
